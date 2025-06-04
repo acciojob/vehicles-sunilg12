e30f018a4f0dd9b9e9f935b8a795f2459ffdcc4b
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-06-04 11:22:44 +0530

    assignment Vechile completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,16 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+
+        F1 formulaOne = new F1();
+        formulaOne.boost();
+        formulaOne.drive();
+        formulaOne.start();
+        formulaOne.stop();
+
+        Boat boat = new Boat("INS Vikrant", 250);
+        System.out.println(boat.getVehicleCapacity());
+        System.out.println(boat.getVehicleName());
+
     }
 }
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -2,27 +2,11 @@ package com.driver;
 
 public class Vehicle {
 
-    private String name;
-    private int currentSpeed;
-    private int currentDirection;
-
-    public Vehicle(String name) {
-
+    public void start() {
+        System.out.println("Vehicle is starting.");
     }
 
-    public void steer(int direction){
-        //direction is in degrees, add it to the current direction
-        System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
+    public void stop() {
+        System.out.println("Vehicle is stopping.");
     }
-
-    public void move(int speed, int direction){
-        //set the values of currentSpeed and currentDirection
-        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
-    }
-
-    public void stop(){
-
-        System.out.println("stop method called - The vehicle is stopped");
-    }
-
 }
