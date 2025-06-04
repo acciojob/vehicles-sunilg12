149530748ffebaff06a82d0d24d716091ecfae0b
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-06-04 13:26:05 +0530

    assignment Vechile completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,15 +3,18 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
 
-        F1 formulaOne = new F1();
-        formulaOne.boost();
-        formulaOne.drive();
-        formulaOne.start();
-        formulaOne.stop();
+        Car ford = new Car();
+        ford.drive();
+        ford.start();
+        ford.stop();
 
-        Boat boat = new Boat("INS Vikrant", 250);
-        System.out.println(boat.getVehicleCapacity());
-        System.out.println(boat.getVehicleName());
+        F1 formulaCar = new F1();
+        formulaCar.start();
+        formulaCar.boost();
+        formulaCar.stop();
 
+        Boat ship = new Boat("INS Vikrant", 250);
+        System.out.println("Boat Name: " + ship.getVehicleName());
+        System.out.println("Boat Capacity: " + ship.getVehicleCapacity());
     }
 }
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,4 @@
 package com.driver.test;
 
-import com.driver.Boat;
-import com.driver.F1;
-import com.driver.WaterVehicle;
-
 public class TestCases {
 }
